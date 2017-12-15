d79b69a462cc0e3adba380ae4e4ab4747481fb47
matias arenhard <matias_arenhard@hotmail.com>
2017-12-15 13:58:44 -0200

    creating models

--- app.rb
+++ app.rb
@@ -3,9 +3,12 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 require './config/database'
+Dir["./app/models/*.rb"].each {|file| require file }
 
 class App < Sinatra::Base
   get '/sinatra' do
     'Hello world Sinatra!'
   end
 end
+
+# Load Models
